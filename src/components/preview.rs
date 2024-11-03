8ff3355e438332ce21c43a2f9cc53193d31f3e07
Fabian Bösiger <boesiger.fabian@outlook.com>
2024-11-03 18:20:51 +0100

    fix previewer

--- src/components/preview.rs
+++ src/components/preview.rs
@@ -9,28 +9,17 @@ pub fn start_preview(form_id: &str) {
     let base_context = expect_context::<BaseContext>();
 
     js_sys::eval(&format!(r#"
-            if (document.getElementById("preview-wrapper").classList.contains("visible")) {{
-                var preview = document.getElementById("preview");
-                preview.innerHTML = "";
-                var paged = new window.Paged.Previewer();
-                window.previewer = paged;
-                paged.preview(
-                    '<div id="print">' + document.getElementById("{}").innerHTML + '</div>',
-                    ["{}"],
-                    preview
-                );
-            }}
+            startPreview("{}", "{}");
         "#,
         form_id,
         base_context.resolve_path("/print.css")
     )).ok();
 }
 
-pub fn stop_preview(form_id: &str) {
+pub fn stop_preview(_form_id: &str) {
     js_sys::eval(&format!(r#"
-        var preview = document.getElementById("{}");
-        window.previewer.removeStyles(preview);
-    "#, form_id)).ok();
+        stopPreview();
+    "#)).ok();
 }
 
 #[component]
@@ -58,11 +47,33 @@ pub fn Preview() -> impl IntoView {
                 }
             }
 
+            async function startPreview(formId, printCss) {
+                if (document.getElementById("preview-wrapper").classList.contains("visible")) {
+                    var preview = document.getElementById("preview");
+                    preview.innerHTML = "";
+                    var paged = new window.Paged.Previewer();
+                    window.previewer = paged;
+                    await paged.preview(
+                        '<div id="print">' + document.getElementById(formId).innerHTML + '</div>',
+                        [printCss],
+                        preview
+                    );
+                    resizePreview();
+                }
+            }
+
+            async function stopPreview() {
+                var preview = document.getElementById("preview");
+                preview.removeAttribute("style");
+                preview.parentElement.removeAttribute("style");
+                window.previewer.removeStyles(preview);
+            }
+
             window.PagedConfig = {
                 auto: false
             };
 
-            window.addEventListener("DOMContentLoaded", function() {
+            /*window.addEventListener("DOMContentLoaded", function() {
                 const previewObserver = new MutationObserver(function(mutations) {
                     mutations.forEach(function(mutation) {
                         resizePreview();
@@ -73,7 +84,7 @@ pub fn Preview() -> impl IntoView {
                     childList: true,
                     subtree: true
                 });
-            });
+            });*/
             
             window.addEventListener("resize", resizePreview);
         "#</Script>
